17c0cef7a1db708a19659492ac4e88143e86748d
Hemant4747 <hemant4720@gmail.com>
2024-02-08 01:58:14 +0530

    first commit

---
+++ gym-website/src/Components/Offer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Offer(){
+    return(
+        <div id="main-offer">
+            <div className="name">
+                <h1>A BIG <span>OFFER </span>FOR THIS WINTER </h1>
+                <p className="details">Lorem ipsum, dolor sit amet consectetur</p>
+                <div className="pr-btns">
+                    <a href="#" className="pr-btn">JOIN NOW </a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Offer;
